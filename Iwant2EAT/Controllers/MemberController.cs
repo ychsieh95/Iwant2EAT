1b14cffe9e8b0b69c406b2575f4b457abec5a4a4
Holey <ychsieh95@gmail.com>
2016-12-30 07:31:59 +0800

    新增會員 Profile 頁面，增加修改信箱、密碼與刪除帳戶功能

--- Iwant2EAT/Controllers/MemberController.cs
+++ Iwant2EAT/Controllers/MemberController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,19 +13,6 @@ namespace Iwant2EAT.Controllers
         public ActionResult Register()
         {
             return View();
-        }
-
-        [HttpGet]
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Logout()
-        {
-            Session.Clear();
-            return Redirect("/Home/Index");
         }
 
         [HttpPost]
@@ -39,6 +27,8 @@ namespace Iwant2EAT.Controllers
                 if (ms.AddMember(member))
                 {
                     Session.Add("Username", member.Username);
+                    Session.Add("EmailAds", member.Email);
+                    Session.Add("LastTime", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
                     return Redirect("/Home/Index");
                 }
                 else
@@ -67,6 +57,12 @@ namespace Iwant2EAT.Controllers
             }
         }
 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult Login(Models.Member member)
         {
@@ -77,6 +73,8 @@ namespace Iwant2EAT.Controllers
             if (memberList.Any(x => x.Username.Equals(member.Username) && x.Password.Equals(member.Password)))
             {
                 Session.Add("Username", member.Username);
+                Session.Add("EmailAds", memberList.Find(x => x.Username.Equals(member.Username)).Email);
+                Session.Add("LastTime", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
                 return Redirect("/Home/Index");
             }
             else
@@ -92,6 +90,100 @@ namespace Iwant2EAT.Controllers
                     return View();
                 }
             }
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return Redirect("/Home/Index");
+        }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            if (Session.Count <= 0)
+            {
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ChangeEmail(string password, string newEmail)
+        {
+            Service.MemberService ms = new Service.MemberService();
+            List<Models.Member> memberList = ms.LoadAllMember();
+            
+            if (memberList.Find(x => x.Username.Equals(Session["Username"].ToString())).Password.Equals(password, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (ms.ChangeEmail(username: Session["Username"].ToString(), email: newEmail))
+                {
+                    ViewBag.ChangeEmailHTML = "<div class=\"alert alert-success\" role=\"alert\">[Failure] 信箱修改成功！</div>";
+                }
+                else
+                {
+                    ViewBag.ChangeEmailHTML = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 信箱修改失敗！</div>";
+                }
+            }
+            else
+            {
+                ViewBag.ChangeEmailHTML = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 密碼驗證錯誤！</div>";
+            }
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public ActionResult ChangePwd(string password, string newPassword)
+        {
+            Service.MemberService ms = new Service.MemberService();
+            List<Models.Member> memberList = ms.LoadAllMember();
+
+            if (memberList.Find(x => x.Username.Equals(Session["Username"].ToString())).Password.Equals(password, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (ms.ChangePassword(username: Session["Username"].ToString(), password: newPassword))
+                {
+                    ViewBag.ChangePwdHTML = "<div class=\"alert alert-success\" role=\"alert\">[Failure] 密碼修改成功！</div>";
+                }
+                else
+                {
+                    ViewBag.ChangePwdHTML = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 密碼修改失敗！</div>";
+                }
+            }
+            else
+            {
+                ViewBag.ChangePwdHTML = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 密碼驗證錯誤！</div>";
+            }
+            return View("Profile");
+        }
+
+        [HttpPost]
+        public ActionResult DelAccount(string password)
+        {
+            Service.MemberService ms = new Service.MemberService();
+            List<Models.Member> memberList = ms.LoadAllMember();
+
+            if (memberList.Find(x => x.Username.Equals(Session["Username"].ToString())).Password.Equals(password, StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (ms.DeleteMember(username: Session["Username"].ToString()))
+                {
+                    ViewBag.DelAccount = "<div class=\"alert alert-success\" role=\"alert\">[Failure] 密碼修改成功！</div>";
+                    Session.Clear();
+                    return Redirect("/Home/Index");
+                }
+                else
+                {
+                    ViewBag.DelAccount = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 密碼修改失敗！</div>";
+                }
+            }
+            else
+            {
+                ViewBag.DelAccount = "<div class=\"alert alert-danger\" role=\"alert\">[Failure] 密碼驗證錯誤！</div>";
+            }
+            return View("Profile");
         }
     }
 }
--- Iwant2EAT/Services/MemberService.cs
+++ Iwant2EAT/Services/MemberService.cs
@@ -32,5 +32,26 @@ namespace Iwant2EAT.Service
             connection.Open();
             return (new System.Data.SqlClient.SqlCommand(string.Format("INSERT INTO Member (Username, Password, Email) VALUES ('{0}', '{1}', '{2}')", member.Username, member.Password, member.Email), connection).ExecuteNonQuery() > 0);
         }
+
+        public bool ChangePassword(string username, string password)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=.; Initial Catalog=Iwant2EAT; Integrated Security=True");
+            connection.Open();
+            return (new System.Data.SqlClient.SqlCommand(string.Format("UPDATE Member SET Password='{0}' WHERE Username='{1}'", password, username), connection).ExecuteNonQuery() > 0);
+        }
+
+        public bool ChangeEmail(string username, string email)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=.; Initial Catalog=Iwant2EAT; Integrated Security=True");
+            connection.Open();
+            return (new System.Data.SqlClient.SqlCommand(string.Format("UPDATE Member SET Email='{0}' WHERE Username='{1}'", email, username), connection).ExecuteNonQuery() > 0);
+        }
+
+        public bool DeleteMember(string username)
+        {
+            var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=.; Initial Catalog=Iwant2EAT; Integrated Security=True");
+            connection.Open();
+            return (new System.Data.SqlClient.SqlCommand(string.Format("DELETE FROM Member WHERE Username='{0}'", username), connection).ExecuteNonQuery() > 0);
+        }
     }
 }
